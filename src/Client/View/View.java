6fd8ea193dcd33c5ff3fbb34487b160211347432
Philip Wester <Philip.Wester@outlook.com>
2019-06-22 16:53:38 +0200

    Made client interactive.

--- src/Client/View/View.java
+++ src/Client/View/View.java
@@ -3,6 +3,7 @@ package Client.View;
 import Common.Filehandler;
 
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 /**
 *   Might need to change name.
@@ -11,21 +12,36 @@ import java.rmi.RemoteException;
 
 public class View implements Runnable {
     private Filehandler filehandler;
+    private Scanner scanner;
+    private boolean takeCommands = true;
 
     public void start(Filehandler filehandler){
         this.filehandler = filehandler;
+        this.scanner = new Scanner(System.in);
 
         new Thread(this).start();
     }
 
     @Override
     public void run() {
-        String testString = null;
-        try {
-            testString = filehandler.testMessage();
-        } catch (RemoteException e) {
-            e.printStackTrace();
+        while (takeCommands){
+            switch (scanner.nextLine().toUpperCase()){
+                case "TEST":
+                    try {
+                        System.out.println(filehandler.testMessage());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "QUIT":
+                    takeCommands = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid command");
+
+            }
+
         }
-        System.out.println(testString);
     }
 }
